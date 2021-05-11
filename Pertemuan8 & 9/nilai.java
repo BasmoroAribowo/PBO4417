79a7c5f8e8f40ebffc9cd503751d52f94f8889b0
BasmoroAribowo <80206024+BasmoroAribowo@users.noreply.github.com>
2021-05-11 21:48:39 +0700

    Add files via upload

---
+++ Pertemuan8 & 9/nilai.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pertemuan8;
+
+/**
+ *
+ * @author Black_Avous
+ */
+public class nilai extends matkul_tambahan {
+
+    
+    public nilai(){
+    };
+    
+    public void InputNilaiMhs(int i, int j, int k){
+        System.out.println("Hasil : ");
+            System.out.println(mhs.get(0));
+            System.out.println(mhs.get(1) +" Di Input "+ nilaimhs.add(i));
+            System.out.println(mhs.get(2) +" Di Input " + nilaimhs.add(j));
+            System.out.println(mhs.get(3) +" Di Input " + nilaimhs.add(k));
+    };
+    
+}
---
+++ Pertemuan8 & 9/univ.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pertemuan8;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Black_Avous
+ */
+public abstract class univ {
+        ArrayList  mhs = new ArrayList();
+        ArrayList  dosen = new ArrayList();
+        ArrayList<String>  matkul_utm = new ArrayList<>();
+        ArrayList<String>  matkul_tam = new ArrayList<>();
+        ArrayList<Integer>  nilaimhs = new ArrayList<>();
+    
+    public abstract void cetak(int i);
+    public abstract void input(String i, int j);
+}
